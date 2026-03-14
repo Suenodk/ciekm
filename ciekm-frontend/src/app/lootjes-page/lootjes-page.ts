ab92dbf1a12ab497f6904dc2e17e66f9dd9d4797
suenodk <suenodekort@gmail.com>
2026-03-14 10:10:56 +0100

    feat: frontend mogelijkheid namen toe te voegen, en worden weergegeven

--- ciekm-frontend/src/app/lootjes-page/lootjes-page.ts
+++ ciekm-frontend/src/app/lootjes-page/lootjes-page.ts
@@ -1,19 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lootjes-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lootjes-page.html',
   styleUrl: './lootjes-page.css',
 })
 export class LootjesPage {
+  readonly nieuweDeelnemerNaam = signal('');
+  readonly deelnemers = signal<string[]>([]);
   readonly getrokkenLootje = signal('');
   readonly #http = inject(HttpClient);
 
   lootjeTrekken(): void {
-    console.log('lootje trekken')
     this.#http.get("http://localhost:3000", { responseType: 'text' })
       .subscribe((x) => this.getrokkenLootje.set(x));
+  }
+
+  deelnemerToevoegen(): void {
+    this.deelnemers.update(p => [...p, this.nieuweDeelnemerNaam()]);
+    this.nieuweDeelnemerNaam.set('');
   }
 }
